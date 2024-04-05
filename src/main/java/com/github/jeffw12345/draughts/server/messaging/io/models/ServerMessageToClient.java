7ee38a32e4df824302e82e11cc4733c6730ae9b5
Jeff Waters <watersjg@gmail.com>
2024-04-05 15:54:14 +0100

    Added @AllArgsConstructor to ServerMessageToClient so that JSON could be converted to a ServerMessageToClient object

--- src/main/java/com/github/jeffw12345/draughts/server/messaging/io/models/ServerMessageToClient.java
+++ src/main/java/com/github/jeffw12345/draughts/server/messaging/io/models/ServerMessageToClient.java
@@ -2,18 +2,22 @@ package com.github.jeffw12345.draughts.server.messaging.io.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.jeffw12345.draughts.game.models.Board;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ServerMessageToClient {
     @JsonProperty("board_state")
-    private final Board board;
+    private Board board;
 
     @JsonProperty("response_type")
-    private final ServerToClientMessageType serverResponseType;
+    private ServerToClientMessageType serverResponseType;
 
     @JsonProperty("client_id")
-    private final String clientId;
+    private String clientId;
 }
